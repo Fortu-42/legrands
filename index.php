84e19d032f90aae136719df30dd4b45aa0c2e4a3
Fortu-42 <fortu_72@hotmail.com>
2017-05-10 17:47:23 -0400

    primerLegrand

---
+++ index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Le Grand Solution</title>
+
+
+    <!--<link rel="stylesheet" href="css/style.css">-->
+
+    <!-- <link rel="stylesheet" href="css/form-elements.css">-->
+    <!--<link rel="stylesheet" href="css/bootstrapValidator.min.css"/>-->
+
+    <!-- Bootstrap -->
+
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="bootstrap/css/bootstrap-theme.min.css">
+
+    <!--<link rel="stylesheet" href="css/bootstrapValidator.min.css"/>-->
+
+    <!--<link rel="stylesheet" href="css/style-form.css">-->
+
+    <!--<link rel="stylesheet" href="css/form-elements.css">-->
+
+    <link rel="stylesheet" href="aos/dist/aos.css">
+
+    <link rel="stylesheet" href="OwlCarousel/dist/assets/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="owlcarousel/dist/assets/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+
+</head>
+
+<body>
+
+
+
+    <header>
+
+
+    <div class="owl-carousel owl-theme owl-loaded">
+
+        <div class="img img-1 item">
+            <div class="pos">
+                <h1>Best Solutions and alternatives for bussiness and startups</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia nisi minima quod iure alias porro non, omnis sunt commodi cupiditate, aut temporibus repudiandae, voluptatum esse. Perspiciatis maxime necessitatibus repellendus sit?</p>
+                <button class="btn btn-primary">Contact Us</button>
+            </div>
+        </div>
+
+        <div class="img img-2 item">
+            <div class="pos">
+                <h1>Marketing Advisors ready to assist you with your brand or product</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia nisi minima quod iure alias porro non, omnis sunt commodi cupiditate, aut temporibus repudiandae, voluptatum esse. Perspiciatis maxime necessitatibus repellendus sit?</p>
+                <button class="btn btn-primary">Contact Us</button>
+            </div>
+        </div>
+
+        <div class="img img-3 item">
+            <div class="pos">
+                <h1>We organize corporate and private events</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia nisi minima quod iure alias porro non, omnis sunt commodi cupiditate, aut temporibus repudiandae, voluptatum esse. Perspiciatis maxime necessitatibus repellendus sit?</p>
+                <button class="btn btn-primary">Contact Us</button>
+            </div>
+        </div>
+
+        <div class="img img-4 item">
+            <div class="pos">
+                <h1>Ask about our personalized budgets</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia nisi minima quod iure alias porro non, omnis sunt commodi cupiditate, aut temporibus repudiandae, voluptatum esse. Perspiciatis maxime necessitatibus repellendus sit?</p>
+                <button class="btn btn-primary">Contact Us</button>
+            </div>
+        </div>
+
+        <div class="img img-5 item">
+            <div class="pos">
+                <h1>We have tha ability and expertise to produce all kinds of media for your brand or product</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia nisi minima quod iure alias porro non, omnis sunt commodi cupiditate, aut temporibus repudiandae, voluptatum esse. Perspiciatis maxime necessitatibus repellendus sit?</p>
+                <button class="btn btn-primary">Contact Us</button>
+            </div>
+        </div>
+
+    </div>
+
+
+
+
+<!-- End slider -->
+
+
+
+
+        <nav class="navbar navbar-default" data-spy="affix" data-offset-top="497">
+            <div class="container-fluid navi">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+                    <a class="navbar-brand" href="#"><i class="fa fa-bus fa-2" aria-hidden="true"></i>
+TecnoBus GPS</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+
+
+                    <ul class="nav navbar-nav navbar-right">
+
+                        <li><a href="#features">Ventajas</a></li>
+                        <li><a href="#contact">Contacto</a></li>
+                        <li><a href="login.php">Iniciar Sesión</a></li>
+
+                        <!-- <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" class="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>-->
+
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+
+
+    </header>
+
+
+    <div style="height:1000px;"></div>
+
+
+    <footer class="credits" class="container-fluid">
+        <p class="author text-center">Crafted with &hearts; in Venezuela by <a href="#">Fortunato</a></p>
+    </footer>
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+
+    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+
+    <script type="text/javascript" src="aos/dist/aos.js"></script>
+
+    <script src="owlcarousel/dist/owl.carousel.min.js"></script>
+
+    <script type="text/javascript" src="js/script.js"></script>
+
+
+    <!-- <script type="text/javascript" src="http://code.jquery.com/jquery-1.10.2.js"></script>  
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script> 
+    <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.3/js/bootstrapValidator.min.js"> </script>-->
+
+    <script>
+    </script>
+    <!-- <script type="text/javascript" src="../js/scripts.js"></script>-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+</body>
+
+</html>
---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    var height = $(window).height();
+    var owlHeight = height * 0.6;
+
+    console.log(owlHeight);
+
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        items: 2,
+        nav: false,
+        autoHeight: false,
+        autoHeightClass: 'owl-height'
+    });
+
+
+
+
+});
